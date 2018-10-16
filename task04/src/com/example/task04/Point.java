0883a3d4426d91d8e5f07ff596a899acb3c1fd62
Dmitry <enorot@bk.ru>
2018-10-16 15:27:40 +0500

    Чербаджи Дмитрий. Задание 04

--- task04/src/com/example/task04/Point.java
+++ task04/src/com/example/task04/Point.java
@@ -6,35 +6,15 @@ import java.math.*;
  * Класс точки на плоскости
  */
 public class Point {
-    int x;
-    int y;
-
-    public Point() {
-    }
+    final int x;
+    final int y;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    void print() {
-        String pointToString = String.format("(%d, %d)", x, y);
-        System.out.println(pointToString);
-    }
-
     public String toString() {
         return String.format("(%d, %d)", x, y);
     }
-
-    void flip() {
-        x = -x;
-        y = -y;
-        int tmp = x;
-        x = y;
-        y = tmp;
-    }
-
-    double distance(Point point) {
-        return Math.sqrt(Math.pow(point.x - x, 2) + Math.pow(point.y - y, 2));
-    }
-}
+} 
