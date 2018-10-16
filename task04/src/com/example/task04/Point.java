624fcb4c9f48c68fb1d71f5a0d7a661c50b3a840
Dmitry <enorot@bk.ru>
2018-10-16 10:58:24 +0500

    Чербаджи Дмитрий. Задание 04

---
+++ task04/src/com/example/task04/Point.java
@@ -0,0 +1,40 @@
+package com.example.task04;
+
+import java.math.*;
+
+/**
+ * Класс точки на плоскости
+ */
+public class Point {
+    int x;
+    int y;
+
+    public Point() {
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void print() {
+        String pointToString = String.format("(%d, %d)", x, y);
+        System.out.println(pointToString);
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
+    void flip() {
+        x = -x;
+        y = -y;
+        int tmp = x;
+        x = y;
+        y = tmp;
+    }
+
+    double distance(Point point) {
+        return Math.sqrt(Math.pow(point.x - x, 2) + Math.pow(point.y - y, 2));
+    }
+}
--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,17 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(2, 2);
+        Line line = new Line(p1, p2);
+        Point p = new Point(5, 5);
 
+        System.out.println("Отрезок: " + line.toString());
+        if (line.isCollinearLine(p)) {
+            System.out.println("Точка лежит на отрезке");
+        }
+        else {
+            System.out.println("Точка не лежит на отрезке");
+        }
     }
 }
